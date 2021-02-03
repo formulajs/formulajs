876585622f83ace54e98ce89cf28cf9024ca8c5b
Nenad Živanović <nenad@zoozdev.com>
2021-02-03 14:23:43 +0100

    Now and today are the same day - diff should be 0

--- test/date-time.js
+++ test/date-time.js
@@ -59,6 +59,7 @@ describe('Date & Time', function () {
     dateTime.DAYS(new Date(1900, 1, 2), new Date(1900, 1, 1)).should.equal(1);
     dateTime.DAYS('a', 1).should.equal(error.value);
     dateTime.DAYS(1, 'a').should.equal(error.value);
+    dateTime.DAYS(dateTime.NOW(), dateTime.TODAY()).should.equal(0);
   });
 
   it('DAYS360', function () {
