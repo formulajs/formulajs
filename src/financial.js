6107a10bad0a73baf9805995b1f51f685c6be739
Andrew <andrewtflores@yahoo.com>
2023-01-04 09:06:25 -0600

    Implement PRICEDISC function and tests

--- src/financial.js
+++ src/financial.js
@@ -1267,24 +1267,87 @@ export function PRICE() {
 }
 
 // TODO
-/**
- * -- Not implemented --
- *
- * Returns the price per $100 face value of a discounted security.
- *
- * Category: Financial
- *
- * @param {*} settlement The security's settlement date. The security settlement date is the date after the issue date when the security is traded to the buyer.
- * @param {*} maturity The security's maturity date. The maturity date is the date when the security expires.
- * @param {*} discount The security's discount rate.
- * @param {*} redemption The security's redemption value per $100 face value.
- * @param {*} basis Optional. The type of day count basis to use.
- * @returns
- */
-export function PRICEDISC() {
-  throw new Error('PRICEDISC is not implemented')
-}
+  /**
+   * Returns the price per $100 face value of a discounted security.
+   *
+   * Category: Financial
+   *
+   * @param {*} settlement The security's settlement date. The security settlement date is the date after the issue date when the security is traded to the buyer.
+   * @param {*} maturity The security's maturity date. The maturity date is the date when the security expires.
+   * @param {*} discount The security's discount rate.
+   * @param {*} redemption The security's redemption value per $100 face value.
+   * @param {*} basis Optional. The type of day count basis to use.
+   * @returns
+   */
+  export function PRICEDISC (
+    settlement,
+    maturity,
+    discount,
+    redemption,
+    basis
+  ) {
+    console.log("Basis before: ", basis);
+    settlement = utils.parseDate(settlement)
+    maturity = utils.parseDate(maturity)
+    discount = utils.parseNumber(discount)
+    redemption = utils.parseNumber(redemption)
+    basis = utils.parseNumber(basis)
 
+    basis = basis || 0;
+
+    if (utils.anyIsError(settlement, maturity, discount, redemption, basis)) {
+      return error.value
+    }
+
+    // Return error if discount is less than or equal to zero
+    if (discount <= 0) {
+      return error.num
+    }
+
+    // Return error if redemption is less than or equal to zero
+    if (redemption <= 0) {
+      return error.num
+    }
+
+    // Return error if basis is neither 0, 1, 2, 3, or 4
+    if ([0, 1, 2, 3, 4].indexOf(basis) === -1) {
+      return error.num
+    }
+
+    // Return error if settlement is greater than maturity
+    if (settlement >= maturity) {
+      return error.value
+    }
+
+    let basisVal = 0
+    if (basis === 0) {
+      // Error: Basis value 0/null is not implemented
+      // throw new Error('US (NASD) 30/360 day count convention is not yet implemented')
+      return error.num
+    } else if (basis === 1) {
+      basisVal = 365
+    } else if (basis === 2) {
+      basisVal = 360
+    } else if (basis === 3) {
+      basisVal = 365
+    } else if (basis === 4) {
+      // Error: Basis value 4 is not implemented
+      // throw new Error('European 30/360 day count convention is not yet implemented')
+      return error.num
+    } else {
+      // Unsupported basis value
+      return error.num
+    }
+
+    // Calculate and return price
+    return (
+      redemption -
+      (discount *
+        redemption *
+        dateTime.DATEDIF(new Date(settlement), new Date(maturity), 'D')) /
+        basisVal
+    );
+  };
 // TODO
 /**
  * -- Not implemented --
@@ -1639,7 +1702,7 @@ export function TBILLYIELD(settlement, maturity, pr) {
 // TODO
 /**
  * -- Not implemented --
- * 
+ *
  * Returns the depreciation of an asset for a specified or partial period by using a declining balance method.
  *
  * Category: Financial
