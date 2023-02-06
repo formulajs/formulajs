92d03981d4ff2d1b14032e20654e6a099c8c281e
Ruan-Miguel <miguelcoelho148@gmail.com>
2023-02-06 09:35:47 -0300

    fix and improve serialNumberToDate function

--- src/utils/common.js
+++ src/utils/common.js
@@ -225,32 +225,13 @@ const excelInitialTime = Date.UTC(1900, 0, 0)
 const excelLeapYearBug = Date.UTC(1900, 1, 29)
 
 export function serialNumberToDate(serial) {
-  if (serial < 60) {
-    serial += 1
+  let jsDateInMilliseconds = excelInitialTime + serial * millisecondsPerDay
+
+  if (jsDateInMilliseconds >= excelLeapYearBug + millisecondsPerDay) {
+    jsDateInMilliseconds -= millisecondsPerDay
   }
 
-  const utc_days = Math.floor(serial - 25569)
-  const utc_value = utc_days * 86400
-  const date_info = new Date(utc_value * 1000)
-  const fractional_day = serial - Math.floor(serial) + 0.0000001
-
-  let total_seconds = Math.floor(86400 * fractional_day)
-
-  const seconds = total_seconds % 60
-
-  total_seconds -= seconds
-
-  const hours = Math.floor(total_seconds / (60 * 60))
-  const minutes = Math.floor(total_seconds / 60) % 60
-  let days = date_info.getUTCDate()
-  let month = date_info.getUTCMonth()
-
-  if (serial >= 60 && serial < 61) {
-    days = 29
-    month = 1
-  }
-
-  return new Date(date_info.getUTCFullYear(), month, days, hours, minutes, seconds)
+  return new Date(jsDateInMilliseconds)
 }
 
 export const dateToSerialNumber = function (jsDate) {
