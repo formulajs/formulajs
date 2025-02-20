formulajs/formulajs

commit 46932749bb39cbb4c8b3caa31eefa73910ac4cfa

Fix/CEILING, CEILING.MATH, FLOOR, FLOOR.MATH (#193)
* Updated dependencies

* Fix CEILING, CEILING.MATH, FLOOR, FLOOR.MATH

* Update/add test cases for CEILING, CEILING.MATH, FLOOR, FLOOR.MATH

src/math-trig.js
@@ -366,10 +366,36 @@ export function BASE(number, radix, min_length) {
  * @param {*} mode Optional. For negative numbers, controls whether Number is rounded toward or away from zero.
  * @returns
  */
-export function CEILING(number, significance, mode) {
+export function CEILING(number, significance) {
+  number = utils.parseNumber(number)
+  significance = utils.parseNumber(significance)
+
+  const anyError = utils.anyError(number, significance)
+
+  if (anyError) {
+    return anyError
+  }
+
+  if (significance === 0) {
+    return 0
+  }
+
+  if (number > 0 && significance < 0) {
+    return error.num
+  }
+
+  return Math.ceil(number / significance) * significance
+}
+
+CEILING.MATH = (number, significance, mode = 0) => {
+  if (significance === undefined) {
+    significance = number > 0 ? 1 : -1
+  }
+
   number = utils.parseNumber(number)
   significance = utils.parseNumber(significance)
   mode = utils.parseNumber(mode)
+
   const anyError = utils.anyError(number, significance, mode)
 
   if (anyError) {
@@ -381,22 +407,17 @@ export function CEILING(number, significance, mode) {
   }
 
   significance = Math.abs(significance)
-  const precision = -Math.floor(Math.log(significance) / Math.log(10))
 
-  if (number >= 0) {
-    return ROUND(Math.ceil(number / significance) * significance, precision)
+  if (mode === 0) {
+    return Math.ceil(number / significance) * significance
   } else {
-    if (mode === 0) {
-      return -ROUND(Math.floor(Math.abs(number) / significance) * significance, precision)
-    } else {
-      return -ROUND(Math.ceil(Math.abs(number) / significance) * significance, precision)
-    }
+    return number > 0
+      ? Math.ceil(number / significance) * significance
+      : Math.floor(number / significance) * significance
   }
 }
 
-CEILING.MATH = CEILING
-
-CEILING.PRECISE = CEILING
+CEILING.PRECISE = CEILING.MATH
 
 /**
  * Returns the number of combinations for a given number of objects.
@@ -642,7 +663,7 @@ export function EVEN(number) {
     return number
   }
 
-  return CEILING(number, -2, -1)
+  return CEILING.MATH(number, -2, -1)
 }
 
 /**
@@ -734,26 +755,22 @@ export function FACTDOUBLE(number) {
 export function FLOOR(number, significance) {
   number = utils.parseNumber(number)
   significance = utils.parseNumber(significance)
+
   const anyError = utils.anyError(number, significance)
 
   if (anyError) {
     return anyError
   }
 
-  if (significance === 0) {
-    return 0
+  if (!significance) {
+    return error.div0
   }
 
-  if (!(number >= 0 && significance > 0) && !(number <= 0 && significance < 0)) {
+  if (number > 0 && significance < 0) {
     return error.num
   }
 
-  significance = Math.abs(significance)
-  const precision = -Math.floor(Math.log(significance) / Math.log(10))
-
-  return number >= 0
-    ? ROUND(Math.floor(number / significance) * significance, precision)
-    : -ROUND(Math.ceil(Math.abs(number) / significance), precision)
+  return Math.floor(number / significance) * significance
 }
 
 // TODO: Verify
@@ -768,16 +785,11 @@ export function FLOOR(number, significance) {
  * @param {*} mode Optional. The direction (toward or away from 0) to round negative numbers.
  * @returns
  */
-FLOOR.MATH = (number, significance, mode) => {
-  if (significance instanceof Error) {
-    return significance
-  }
-
-  significance = significance === undefined ? 0 : significance
-
+FLOOR.MATH = (number, significance = 1, mode = 0) => {
   number = utils.parseNumber(number)
   significance = utils.parseNumber(significance)
   mode = utils.parseNumber(mode)
+
   const anyError = utils.anyError(number, significance, mode)
 
   if (anyError) {
@@ -788,16 +800,15 @@ FLOOR.MATH = (number, significance, mode) => {
     return 0
   }
 
-  significance = significance ? Math.abs(significance) : 1
-  const precision = -Math.floor(Math.log(significance) / Math.log(10))
+  significance = Math.abs(significance)
 
-  if (number >= 0) {
-    return ROUND(Math.floor(number / significance) * significance, precision)
-  } else if (mode === 0 || mode === undefined) {
-    return -ROUND(Math.ceil(Math.abs(number) / significance) * significance, precision)
+  if (mode === 0) {
+    return Math.floor(number / significance) * significance
+  } else {
+    return number > 0
+      ? Math.floor(number / significance) * significance
+      : Math.ceil(number / significance) * significance
   }
-
-  return -ROUND(Math.floor(Math.abs(number) / significance) * significance, precision)
 }
 
 // Deprecated
@@ -811,7 +822,7 @@ FLOOR.MATH = (number, significance, mode) => {
  * @param {*} significance Optional. The multiple to which number is to be rounded. If significance is omitted, its default value is 1.
  * @returns
  */
-FLOOR.PRECISE = FLOOR['MATH']
+FLOOR.PRECISE = FLOOR.MATH
 
 // adapted http://rosettacode.org/wiki/Greatest_common_divisor#JavaScript
 /**

test/math-trig.js
@@ -128,9 +128,11 @@ describe('Math & Trig', () => {
   })
 
   it('CEILING', () => {
+    expect(mathTrig.CEILING()).to.equal(0)
     expect(mathTrig.CEILING(undefined)).to.equal(0)
     expect(mathTrig.CEILING(error.na)).to.equal(error.na)
     expect(mathTrig.CEILING(1, error.na)).to.equal(error.na)
+    expect(mathTrig.CEILING(error.na, error.na)).to.equal(error.na)
     expect(mathTrig.CEILING(4.1)).to.equal(0)
     expect(mathTrig.CEILING(4.1, 1)).to.equal(5)
     expect(mathTrig.CEILING(-4.1, 1)).to.equal(-4)
@@ -138,34 +140,45 @@ describe('Math & Trig', () => {
     expect(mathTrig.CEILING(4.1, 1)).to.equal(5)
     expect(mathTrig.CEILING(4.1, 2)).to.equal(6)
     expect(mathTrig.CEILING(-4.1, 2)).to.equal(-4)
-    expect(mathTrig.CEILING(-4.1, -2)).to.equal(-4)
+    expect(mathTrig.CEILING(-4.1, -2)).to.equal(-6)
+    expect(mathTrig.CEILING(1000, 15)).to.equal(1005)
+    expect(mathTrig.CEILING(1000, 10.5)).to.equal(1008)
+    expect(mathTrig.CEILING(7.7, 0.24)).to.equal(7.92)
     expect(mathTrig.CEILING(1.234, 0.1)).to.approximately(1.3, 1e-9)
     expect(mathTrig.CEILING(-1.234, 0.1)).to.approximately(-1.2, 1e-9)
-    expect(mathTrig.CEILING(-1.234, -0.1)).to.approximately(-1.2, 1e-9)
-    expect(mathTrig.CEILING(-1.234, -0.01)).to.approximately(-1.23, 1e-9)
+    expect(mathTrig.CEILING(-1.234, -0.1)).to.approximately(-1.3, 1e-9)
+    expect(mathTrig.CEILING(-1.234, -0.01)).to.approximately(-1.24, 1e-9)
     expect(mathTrig.CEILING(-1.234, -0.001)).to.approximately(-1.234, 1e-9)
-    expect(mathTrig.CEILING(-4.1, 2, 0)).to.equal(-4)
-    expect(mathTrig.CEILING(-4.1, 2, -1)).to.equal(-6)
-    expect(mathTrig.CEILING(-4.1, -2, 0)).to.equal(-4)
-    expect(mathTrig.CEILING(-4.1, -2, -1)).to.equal(-6)
-    expect(mathTrig.CEILING(-4.1, -2, 'invalid')).to.equal(error.value)
   })
 
   it('CEILING.MATH', () => {
     expect(mathTrig.CEILING.MATH(undefined)).to.equal(0)
     expect(mathTrig.CEILING.MATH(error.na)).to.equal(error.na)
     expect(mathTrig.CEILING.MATH(24.3, 5)).to.equal(25)
-    expect(mathTrig.CEILING.MATH(6.7)).to.equal(0)
+    expect(mathTrig.CEILING.MATH(6.7)).to.equal(7)
     expect(mathTrig.CEILING.MATH(6.7, 1)).to.equal(7)
     expect(mathTrig.CEILING.MATH(-8.1, 2)).to.equal(-8)
     expect(mathTrig.CEILING.MATH(-5.5, 2, -1)).to.equal(-6)
     expect(mathTrig.CEILING.MATH(-5.5, 2, 'invalid')).to.equal(error.value)
+    expect(mathTrig.CEILING.MATH(-4.1, 2, 0)).to.equal(-4)
+    expect(mathTrig.CEILING.MATH(-4.1, 2, -1)).to.equal(-6)
+    expect(mathTrig.CEILING.MATH(-4.1, -2, 0)).to.equal(-4)
+    expect(mathTrig.CEILING.MATH(-4.1, -2, -1)).to.equal(-6)
+    expect(mathTrig.CEILING.MATH(-4.1, -2, 'invalid')).to.equal(error.value)
+    expect(mathTrig.CEILING.MATH(1000, 15)).to.equal(1005)
+    expect(mathTrig.CEILING.MATH(1000, 10.5)).to.equal(1008)
+    expect(mathTrig.CEILING.MATH(7.7, 0.24)).to.equal(7.92)
+    expect(mathTrig.CEILING.MATH(-5.5, 2, 1)).to.equal(-6)
+    expect(mathTrig.CEILING.MATH(210.67)).to.equal(211)
+    expect(mathTrig.CEILING.MATH(-5.5, 2, 1)).to.equal(-6)
+    expect(mathTrig.CEILING.MATH(-5.5, 0.66, -1)).to.equal(-5.94)
+    expect(mathTrig.CEILING.MATH(-5.5, 0.33, 1)).to.equal(-5.61)
   })
 
   it('CEILING.PRECISE', () => {
     expect(mathTrig.CEILING.PRECISE(undefined)).to.equal(0)
     expect(mathTrig.CEILING.PRECISE(error.na)).to.equal(error.na)
-    expect(mathTrig.CEILING.PRECISE(4.3)).to.equal(0)
+    expect(mathTrig.CEILING.PRECISE(4.3)).to.equal(5)
     expect(mathTrig.CEILING.PRECISE(4.3, 1)).to.equal(5)
     expect(mathTrig.CEILING.PRECISE(-4.3, 1)).to.equal(-4)
     expect(mathTrig.CEILING.PRECISE(4.3, 2)).to.equal(6)
@@ -309,8 +322,8 @@ describe('Math & Trig', () => {
   })
 
   it('FLOOR', () => {
-    expect(mathTrig.FLOOR(undefined, undefined)).to.equal(0)
-    expect(mathTrig.FLOOR(2, undefined)).to.equal(0) // different than Excel
+    expect(mathTrig.FLOOR(undefined, undefined)).to.equal(error.div0)
+    expect(mathTrig.FLOOR(2, undefined)).to.equal(error.div0) // different than Excel
     expect(mathTrig.FLOOR(undefined, 2)).to.equal(0)
     expect(mathTrig.FLOOR(error.na)).to.equal(error.na)
     expect(mathTrig.FLOOR(3.7, 2)).to.equal(2)
@@ -318,13 +331,17 @@ describe('Math & Trig', () => {
     expect(mathTrig.FLOOR(2.5, -2)).to.equal(error.num)
     expect(mathTrig.FLOOR(1.58, 0.1)).to.approximately(1.5, 1e-9)
     expect(mathTrig.FLOOR(0.234, 0.01)).to.approximately(0.23, 1e-9)
-    expect(mathTrig.FLOOR(0.234, 0)).to.equal(0)
+    expect(mathTrig.FLOOR(0.234, 0)).to.equal(error.div0)
     expect(mathTrig.FLOOR('invalid', 0)).to.equal(error.value)
+    expect(mathTrig.FLOOR(1000, 15)).to.equal(990)
+    expect(mathTrig.FLOOR(1000, 10.5)).to.equal(997.5)
+    expect(mathTrig.FLOOR(7.7, 0.24)).to.equal(7.68)
+    expect(mathTrig.FLOOR(-2.5, -2)).to.equal(-2)
   })
 
   it('FLOOR.PRECISE', () => {
     expect(mathTrig.FLOOR.PRECISE(undefined, undefined)).to.equal(0)
-    expect(mathTrig.FLOOR.PRECISE(2, undefined)).to.equal(0)
+    expect(mathTrig.FLOOR.PRECISE(2, undefined)).to.equal(2)
     expect(mathTrig.FLOOR.PRECISE(undefined, 2)).to.equal(0)
     expect(mathTrig.FLOOR.PRECISE(error.na)).to.equal(error.na)
     expect(mathTrig.FLOOR.PRECISE(2014.6, 0.2)).to.equal(2014.4)
@@ -332,28 +349,29 @@ describe('Math & Trig', () => {
     expect(mathTrig.FLOOR.PRECISE(3.2, 1)).to.equal(3)
     expect(mathTrig.FLOOR.PRECISE(-3.2, 1)).to.equal(-4)
     expect(mathTrig.FLOOR.PRECISE(3.2, -1)).to.equal(3)
-    expect(mathTrig.FLOOR.PRECISE(3.2)).to.equal(0)
+    expect(mathTrig.FLOOR.PRECISE(3.2)).to.equal(3)
   })
 
   it('FLOOR.MATH', () => {
     expect(mathTrig.FLOOR.MATH(undefined, undefined)).to.equal(0)
-    expect(mathTrig.FLOOR.MATH(2, undefined)).to.equal(0)
+    expect(mathTrig.FLOOR.MATH(2, undefined)).to.equal(2)
     expect(mathTrig.FLOOR.MATH(undefined, 2)).to.equal(0)
     expect(mathTrig.FLOOR.MATH(error.na)).to.equal(error.na)
     expect(mathTrig.FLOOR.MATH(24.3, 5)).to.equal(20)
-    expect(mathTrig.FLOOR.MATH(6.7)).to.equal(0)
+    expect(mathTrig.FLOOR.MATH(6.7)).to.equal(6)
     expect(mathTrig.FLOOR.MATH(-8.1, 2)).to.equal(-10)
     expect(mathTrig.FLOOR.MATH(-8.1, 0)).to.equal(0)
     expect(mathTrig.FLOOR.MATH(-5.5, 2, -1)).to.equal(-4)
     expect(mathTrig.FLOOR.MATH('invalid', 0)).to.equal(error.value)
-
     expect(mathTrig.FLOOR.MATH(-3.2, -1)).to.equal(-4)
     expect(mathTrig.FLOOR.MATH(3.2, 1)).to.equal(3)
     expect(mathTrig.FLOOR.MATH(-3.2, 1)).to.equal(-4)
     expect(mathTrig.FLOOR.MATH(3.2, -1)).to.equal(3)
-    expect(mathTrig.FLOOR.MATH(3.2)).to.equal(0)
+    expect(mathTrig.FLOOR.MATH(3.2)).to.equal(3)
     expect(mathTrig.FLOOR.MATH(3.2, 0)).to.equal(0)
     expect(mathTrig.FLOOR.MATH(3.2, 'invalid')).to.equal(error.value)
+    expect(mathTrig.FLOOR.MATH(-2.5, -2)).to.equal(-4)
+    expect(mathTrig.FLOOR.MATH(-2.5, -2, 1)).to.equal(-2)
   })
 
   it('GCD', () => {
@@ -384,9 +402,9 @@ describe('Math & Trig', () => {
     expect(mathTrig.ISO.CEILING(4.3, 1)).to.equal(5)
     expect(mathTrig.ISO.CEILING(-4.3, 1)).to.equal(-4)
     expect(mathTrig.ISO.CEILING(4.3, 2)).to.equal(6)
-    expect(mathTrig.ISO.CEILING(4.3, -2)).to.equal(6)
+    expect(mathTrig.ISO.CEILING(4.3, -2)).to.equal(error.num)
     expect(mathTrig.ISO.CEILING(-4.3, 2)).to.equal(-4)
-    expect(mathTrig.ISO.CEILING(-4.3, -2)).to.equal(-4)
+    expect(mathTrig.ISO.CEILING(-4.3, -2)).to.equal(-6)
     expect(mathTrig.ISO.CEILING(-4.3, 'invalid')).to.equal(error.value)
   })
 

commit e1511c740ee50fb319c8e3db8a0cf3949c8d1bee

Fix Types regression in #193 (#201)
* Updated dependencies

* Added target value to compilerOptions

* Fixes export of MATH and PRECISE in CEILING and FLOOR functions

* Updated dependencies

* Removed unnecessary dependency

src/math-trig.js
@@ -387,6 +387,16 @@ export function CEILING(number, significance) {
   return Math.ceil(number / significance) * significance
 }
 
+/**
+ * Rounds a number up, to the nearest integer or to the nearest multiple of significance.
+ *
+ * Category: Math and trigonometry
+ *
+ * @param {*} number The value you want to round.
+ * @param {*} significance Optional. This is the number of significant digits after the decimal point to which number is to be rounded.
+ * @param {*} mode Optional. For negative numbers, controls whether Number is rounded toward or away from zero.
+ * @returns
+ */
 CEILING.MATH = (number, significance, mode = 0) => {
   if (significance === undefined) {
     significance = number > 0 ? 1 : -1
@@ -417,7 +427,18 @@ CEILING.MATH = (number, significance, mode = 0) => {
   }
 }
 
-CEILING.PRECISE = CEILING.MATH
+/**
+ * Rounds a number up, to the nearest integer or to the nearest multiple of significance.
+ *
+ * Category: Math and trigonometry
+ *
+ * @param {*} number The value you want to round.
+ * @param {*} significance Optional. This is the number of significant digits after the decimal point to which number is to be rounded.
+ * @returns
+ */
+CEILING.PRECISE = (number, significance) => {
+  return CEILING.MATH(number, significance)
+}
 
 /**
  * Returns the number of combinations for a given number of objects.
@@ -822,7 +843,9 @@ FLOOR.MATH = (number, significance = 1, mode = 0) => {
  * @param {*} significance Optional. The multiple to which number is to be rounded. If significance is omitted, its default value is 1.
  * @returns
  */
-FLOOR.PRECISE = FLOOR.MATH
+FLOOR.PRECISE = (number, significance) => {
+  return FLOOR.MATH(number, significance)
+}
 
 // adapted http://rosettacode.org/wiki/Greatest_common_divisor#JavaScript
 /**