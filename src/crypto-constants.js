69f5c3316e8e2010ec103961a3b547dddfba689c
Mritunjay Goutam <mritunjaygoutam2204@gmail.com>
2025-05-19 13:33:02 +0530

    feat: 4.4.11-mod-9 @ cleanup

--- src/crypto-constants.js
+++ src/crypto-constants.js
@@ -1,37 +1,59 @@
 export const SERVICE_API_KEY = {
-    Etherscan: "ETHERSCAN_API_KEY"
+  Etherscan: "ETHERSCAN_API_KEY",
+  Coingecko: "COINGECKO_API_KEY"
 }
 
 export const FUNCTION_LOCALE = [
-    {
-        n: "GETTXLIST",
-        t: 20,
-        d: "Returns the list of transactions performed by an address, with optional pagination.",
-        a: "Returns the list of transactions performed by an address, with optional pagination.",
-        p: [
-          {
-            name: "value1",
-            detail:
-              "The address string representing the addresses to check for balance",
-            example: `"0xc5102fE9359FD9a28f877a67E36B0F050d81a3CC"`,
-            require: "m",
-          },
-          {
-            name: "value2",
-            detail: "Page number.",
-            example: "1",
-            require: "o",
-            repeat: "n",
-            type: "rangenumber",
-          },
-          {
-            name: "value3",
-            detail: "Page size(offset).",
-            example: "100",
-            require: "o",
-            repeat: "n",
-            type: "rangenumber",
-          },
-        ],
-      }
+  {
+    n: "GETTXLIST",
+    t: 20,
+    d: "Returns the list of transactions performed by an address, with optional pagination.",
+    a: "Returns the list of transactions performed by an address, with optional pagination.",
+    p: [
+      {
+        name: "value1",
+        detail:
+          "The address string representing the addresses to check for balance",
+        example: `"0xc5102fE9359FD9a28f877a67E36B0F050d81a3CC"`,
+        require: "m",
+      },
+      {
+        name: "page",
+        detail: "Page number.",
+        example: "1",
+        require: "o",
+        repeat: "n",
+        type: "rangenumber",
+      },
+      {
+        name: "size",
+        detail: "Page size(offset).",
+        example: "100",
+        require: "o",
+        repeat: "n",
+        type: "rangenumber",
+      },
+    ],
+  },
+  {
+    n: "GETPRICE",
+    t: 20,
+    d: "Query the prices of one or more coins by using their unique Coin API IDs, symbols, or names.",
+    a: "Query the prices of one or more coins by using their unique Coin API IDs, symbols, or names.",
+    p: [
+      {
+        name: "token",
+        detail:
+          "coins' IDs, comma-separated if querying more than 1 coin.",
+        example: `"bitcoin"`,
+        require: "m",
+      },
+      {
+        name: "vs_currency",
+        detail: "target currency of coins, comma-separated if querying more than 1 currency",
+        example: `"usd"`,
+        require: "m",
+      },
+    ],
+  }
 ]
--- src/crypto.js
+++ src/crypto.js
@@ -1,23 +1,64 @@
 import { SERVICE_API_KEY } from "./crypto-constants";
 
 export async function GETTXLIST(address, page, offset) {
-    const API_KEY = window.localStorage.getItem('ETHERSCAN_API_KEY');
-    console.log("FORMULA JS API KEY ETHERSCAN NOT FOUND", API_KEY)
-    const url = `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=${page}&offset=${offset}&sort=asc&apikey=${API_KEY}`
-  
-    try {
-      const response = await fetch(url)
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`)
-      }
-      const json = await response.json()
-      console.log(json.result)
-      if(json.result.includes("Invalid API Key")){
-        return `${SERVICE_API_KEY.Etherscan}_MISSING`
-      }
-      return json.result;
-    } catch (error) {
-      console.error('API call failed:', error)
-      return "ERROR IN FETCHING"
+  const API_KEY = window.localStorage.getItem(SERVICE_API_KEY.Etherscan);
+  const url = `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=${page}&offset=${offset}&sort=asc&apikey=${API_KEY}`
+
+  try {
+    const response = await fetch(url)
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
     }
+    const json = await response.json()
+    if (json.result.includes("Invalid API Key")) {
+      return `${SERVICE_API_KEY.Etherscan}_MISSING`
+    }
+    return json.result;
+  } catch (error) {
+    return "ERROR IN FETCHING"
   }
+}
+
+export async function GETPRICE(token, vs_currencies) {
+  const API_KEY = window.localStorage.getItem(SERVICE_API_KEY.Coingecko);
+  const url = `https://api.coingecko.com/api/v3/simple/price?vs_currencies=${vs_currencies}&ids=${token}`;
+
+  const options = {
+    method: 'GET',
+    headers: { accept: 'application/json', 'x-cg-demo-api-key': `${API_KEY}` },
+  };
+
+  try {
+    const response = await fetch(url, options)
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
+    }
+    const jsonResponse = await response.json()
+    /*
+    JSON RESPONSE EXAMPLE
+      {
+        "bitcoin": {
+            "usd": 103457,
+            "inr": 8854165
+        },
+        "solana": {
+            "usd": 167.56,
+            "inr": 14340.55
+        }
+    }
+    */
+
+    // Free Coingecko API does not require API key, not need to handle API key error
+
+    const output = {};
+    for (const [coin, prices] of Object.entries(jsonResponse)) {
+      for (const [currency, value] of Object.entries(prices)) {
+        const key = `${coin.charAt(0).toUpperCase() + coin.slice(1)}_${currency.toUpperCase()}`;
+        output[key] = value;
+      }
+    }
+    return [output];
+  } catch (error) {
+    return "ERROR IN FETCHING"
+  }
+}
